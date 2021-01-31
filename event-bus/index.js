556586ec6e1bca8ba2a1486423940c23dfb4853b
Joano <joan.manuel.docarmo@gmail.com>
2021-01-31 12:38:43 -0300

    working

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,7 +12,7 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4008/events', event);
   axios.post('http://localhost:4001/events', event);
   axios.post('http://localhost:4002/events', event);
   axios.post('http://localhost:4003/events', event);
--- posts/index.js
+++ posts/index.js
@@ -40,6 +40,6 @@ app.post('/events', (req, res) => {
   res.send({});
 });
 
-app.listen(4000, () => {
-  console.log('Listening on 4000');
+app.listen(4008, () => {
+  console.log('Listening on 4008');
 });
